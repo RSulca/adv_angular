8223faad1958eb193d0cb69c63b96d1147d5a557
Sulca97 <raul_97_02@hotmail.com>
2020-07-06 17:04:36 -0500

    Basic templates with login/register and some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    Graphics1Component,
+    ProgressComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    NopagefoundComponent,
+    SidebarComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
